5a72a85f2588a150e40edadf657c7766c9603f28
Allanpaul08 <96708752+Allanpaul08@users.noreply.github.com>
2022-12-29 16:00:06 +0800

    Update app.py

--- demo/app.py
+++ demo/app.py
@@ -60,7 +60,7 @@ st.write('\n')
 st.subheader("Experience & Qulifications")
 st.write(
     """
-- ✔️ 7 Years expereince extracting actionable insights from data
+- ✔️ 1 Years expereince extracting actionable insights from data
 - ✔️ Strong hands on experience and knowledge in Python and Excel
 - ✔️ Good understanding of statistical principles and their respective applications
 - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
